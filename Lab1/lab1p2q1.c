3966dca8c5246da0b49d9bac2d8594d387964802
Carlo <CarloMehegan@gmail.com>
2023-09-27 19:19:57 -0400

    Create lab1p2q1.c

---
+++ Lab1/lab1p2q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void outputString()
+{
+    printf("One half is 50%%\n");
+}
+
+void outputIntegerDifference(int num1, int num2)
+{
+    int difference = num1 - num2;
+    printf("The difference between %d and %d is %d\n", num1, num2, difference);
+}
+
+void outputFloatDivision(float num1, float num2)
+{
+    float result = num1 / num2;
+    printf("%.6f / %.6f is %.6f\n", num1, num2, result);
+}
+
+int main()
+{
+    // call the three functions
+    outputString();
+    outputIntegerDifference(10, 3);
+    outputFloatDivision(1.0, 3.0);
+
+    // // print a newline at the end
+    // printf("\n");
+
+    return 0;
+}
